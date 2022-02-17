4b931cf846c6aa766446b331b039a49fcc1a7b57
Emotet_Hunter <69321294+Emotet-hunter@users.noreply.github.com>
2022-02-17 17:48:45 +0100

    Update ordered.sh

--- ordered.sh
+++ ordered.sh
@@ -4,12 +4,19 @@
 bash ./mate_config.sh
 
 #Bright Driver Setup
+#Rewirte <LOCALUSER> 
 sudo bash ./drivers/bright_drivers_setup.sh
 
 #Nvidia Driver
-bash ./drivers/nvidia_setup_0.sh
-bash ./drivers/nvidia_setup_1.sh
+#bash ./drivers/nvidia_setup_laptop_0.sh
+#bash ./drivers/nvidia_setup_laptop_1.sh
+
+#OR
+
+bash ./drivers/nvidia_setup_desktop_0.sh
+bash ./drivers/nvidia_setup_desktop_1.sh
 
 #Pro setup
+#Rewrite <LOCALUSER>
 sudo bash ./setup.sh
 
